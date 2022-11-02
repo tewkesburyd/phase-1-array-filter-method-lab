8d70c0e9c403907566bcabf03ee2c2d2e3439bff
David Tewkesbury <tewkesburyd@hotmail.com>
2022-11-01 22:03:57 -0600

    commit tree baf1bcd289bdffb2a90a85b9a912b85651e37448

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const findMatching = (names, string) => {
+    const name = names.filter((e) => {
+        return e.toLowerCase() === string.toLowerCase()
+        })
+    return name;
+}
+
+const fuzzyMatch = (names, string) => {
+    const name = names.filter((e) => {
+        return e.startsWith(string)
+        })
+    return name;
+}
+
+const matchName = (arrayOfObjs, string) => {
+    const name = arrayOfObjs.filter((e) => {
+        if (e['name'] === string){
+            return e;
+        }
+    })
+    return name;
+}
